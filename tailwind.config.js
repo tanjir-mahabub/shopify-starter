b10c069dd4ed1c0a331636d3fc9693928857efce
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2024-11-01 20:48:29 +0600

    Tailwindcss integrated

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./layout/**/*.liquid', './sections/**/*.liquid', './snippets/**/*.liquid', './templates/**/*.liquid'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
